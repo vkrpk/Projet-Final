26bd2fe42fc53a328da55c6efe3dbca181a31862
Vic <vic_-@live.fr>
2021-04-02 13:54:24 +0200

    debug pack

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -30,4 +30,23 @@ class HomeController extends AbstractController
             'jeu' => $jeu
         ]);
     }
+
+    /**
+     * @Route("/chercher", name="home_chercher")
+     */
+    public function chercher(JeuRepository $jeuRepository, Request $request, FormFactoryInterface $formFactory): Response
+    {
+        $chercher = new Chercher();
+
+        $form = $formFactory->create(ChercherType::class, $chercher);
+
+        $form->handleRequest($request);
+
+        $jeu = $jeuRepository->chercherJeu($chercher);
+        // dd($jeu);
+        return $this->render('chercher.html.twig', [
+            'jeux' => $jeu,
+            'form' => $form->createView()
+        ]);
+    }
 }
