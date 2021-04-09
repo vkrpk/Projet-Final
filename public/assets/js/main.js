afae78e7fdd9a0e021e384335eb6e50266edb890
Tazxor777 <t.ozgul@hotmail.fr>
2021-04-09 11:45:51 +0200

    darkmode

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -12,3 +12,15 @@ function afficherMenuDeroulant(){
 for(let i = 0; i < iconeMenu.length; i++){
     iconeMenu[i].addEventListener("click", afficherMenuDeroulant);
 }
+
+
+const dMode = document.getElementsByClassName('DM');
+const buttonDarkMode = document.getElementById('btn-darkmode');
+
+
+
+function activerDarkMode() {
+for(let i = 0; i < dMode.length; i++){
+    dMode[i].classList.toggle("darkmode");}}
+   
+    buttonDarkMode.addEventListener("click", activerDarkMode);
